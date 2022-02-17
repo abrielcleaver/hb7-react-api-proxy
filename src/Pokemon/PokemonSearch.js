a52363df10937cfb607b731fe24441e018e78706
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-17 13:24:23 -0700

    Update pokemon search component

--- src/Pokemon/PokemonSearch.js
+++ src/Pokemon/PokemonSearch.js
@@ -25,10 +25,10 @@ export default function PokemonSearch() {
     <section className='pokemon'>
       {/* make the fetch on submit */}
       <form onSubmit={handlePokemonSubmit} >
-            Search pokemon for a city
+            Search through our pokedex
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. 
         Don't forget to use the value property to sync these up with the default values in react state */}
-        <label>City
+        <label>
           <input />
         </label>
         <button> Search pokemon</button>
