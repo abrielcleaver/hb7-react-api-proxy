71ae224642fbfa32c8093462fccc9605201e0868
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-17 21:58:26 -0700

    Update BussinesesList component

--- src/Yelp/BusinessesList.js
+++ src/Yelp/BusinessesList.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function BusinessesList() {
   return (
-    <div>BusinessesList</div>
+    <div>
+      {/* {businesses.map((business, i) => <div className="yelp-item" key={business.name + i}>
+        <p>{business.name}</p>
+        <img src={business.image_url} />
+      </div>)} */}
+    </div>
   );
 }
