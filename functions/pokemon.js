87a9c735cdf3f2fa29251dc686f44db9f5062bb1
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-17 20:44:28 -0700

    Add await fetch to pokemon function

--- functions/pokemon.js
+++ functions/pokemon.js
@@ -13,11 +13,13 @@ exports.handler = async (e) => {
     const resp = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${e.queryStringParameters.search}`);
 
     const json = await resp.json();
+
     return { 
       statusCode: 200, 
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the pokemon data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
       body: JSON.stringify(json),
     };
+
   } catch (error) {
     // console.log(error);
     return {
